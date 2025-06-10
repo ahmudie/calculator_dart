83eb84a8489384a7ca503bcb67636fa72e774057
ahmudie <ahmudie@live.de>
2025-06-10 23:52:22 +0200

    changed calcButton into factory method

--- lib/CalculatorView.dart
+++ lib/CalculatorView.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:state_management/calcButton.dart';
-import 'package:math_expressions/math_expressions.dart';
+import 'package:provider/provider.dart';
+import 'CalculatorProvider.dart';
 
 class CalculatorView extends StatefulWidget{
   const CalculatorView({Key? key}) : super(key: key);
@@ -11,208 +12,151 @@ class CalculatorView extends StatefulWidget{
 class _CalculatorViewState extends State<CalculatorView> {
   Widget build(BuildContext context){
 
-    return Scaffold(
-      backgroundColor: Colors.black54,
-      appBar: AppBar(
-        elevation: 0,
+    return ChangeNotifierProvider(
+      create: (_) => CalculatorProvider(),
+      child: Scaffold(
         backgroundColor: Colors.black54,
-        leading: const Icon(Icons.settings, color: Colors.orange,),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(top:18.0),
-            child: Text('DEG', style: TextStyle(color: Colors.white38)),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.black54,
+          leading: const Icon(Icons.settings, color: Colors.orange,),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(top:18.0),
+              child: Text('DEG', style: TextStyle(color: Colors.white38)),
 
 
-          ),
-          SizedBox(width: 20),
-        ],
-      ),
-      body: SafeArea(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Align(
-          alignment: Alignment.bottomRight,
+            ),
+            SizedBox(width: 20),
+          ],
+        ),
+        body: SafeArea(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(result,
-                            textAlign: TextAlign.left,
-                            style: const TextStyle(
-                                color: Colors.white, fontSize:80))),
-                    const Icon(Icons.more_vert,
-                        color: Colors.orange, size: 30),
-                    const SizedBox(width: 20),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(equation,
-                          style: const TextStyle(
-                            fontSize: 40,
-                            color: Colors.white38,
-                          )),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.backspace_outlined,
-                          color: Colors.orange, size: 30),
-                      onPressed: () {
-                        buttonPressed("⌫");
-                      },
-                    ),
-                    const SizedBox(width: 20),
-                  ],
-                ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('AC', Colors.white10, () => buttonPressed('AC')),
-                  calcButton('%', Colors.white10, () => buttonPressed('%')),
-                  calcButton('÷', Colors.white10, () => buttonPressed('÷')),
-                  calcButton("×", Colors.white10, () => buttonPressed('×')),
-                ],
-              ),
-              const SizedBox(height: 10),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('7', Colors.white24, () => buttonPressed('7')),
-                  calcButton('8', Colors.white24, () => buttonPressed('8')),
-                  calcButton('9', Colors.white24, () => buttonPressed('9')),
-                  calcButton('-', Colors.white10, () => buttonPressed('-')),
-                ],
-              ),
-              const SizedBox(height: 10),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('4', Colors.white24, () => buttonPressed('4')),
-                  calcButton('5', Colors.white24, () => buttonPressed('5')),
-                  calcButton('6', Colors.white24, () => buttonPressed('6')),
-                  calcButton('+', Colors.white10, () => buttonPressed('+')),
-                ],
-              ),
-              const SizedBox(height: 10),
-              // calculator number buttons
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    //mainAxisAlignment: MainAxisAlignment.spaceAround
-                    children: [
-                      Row(
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: Consumer<CalculatorProvider>(
+                      builder: (context, calculator, child) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          calcButton(
-                              '1', Colors.white24, () => buttonPressed('1')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.025),
-                          calcButton(
-                              '2', Colors.white24, () => buttonPressed('2')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.025),
-                          calcButton(
-                              '3', Colors.white24, () => buttonPressed('3')),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Text(calculator.result,
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize:80))),
+                              const Icon(Icons.more_vert,
+                                  color: Colors.orange, size: 30),
+                              const SizedBox(width: 20),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Text(calculator.equation,
+                                    style: const TextStyle(
+                                      fontSize: 40,
+                                      color: Colors.white38,
+                                    )),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.backspace_outlined,
+                                    color: Colors.orange, size: 30),
+                                onPressed: () {
+                                  buttonPressed("⌫");
+                                },
+                              ),
+                              const SizedBox(width: 20),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CalcButtonFactory.create(text: 'AC', color: Colors.white24, onPressed: () => buttonPressed('AC')),
+                              CalcButtonFactory.create(text: '%', color: Colors.white24, onPressed: () => buttonPressed('%')),
+                              CalcButtonFactory.create(text: '÷', color: Colors.white24, onPressed: () => buttonPressed('÷')),
+                              CalcButtonFactory.create(text: 'x', color: Colors.white24, onPressed: () => buttonPressed('x')),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CalcButtonFactory.create(text: '7', color: Colors.white10, onPressed: () => buttonPressed('7')),
+                              CalcButtonFactory.create(text: '8', color: Colors.white10, onPressed: () => buttonPressed('8')),
+                              CalcButtonFactory.create(text: '9', color: Colors.white10, onPressed: () => buttonPressed('9')),
+                              CalcButtonFactory.create(text: '-', color: Colors.white24, onPressed: () => buttonPressed('-')),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CalcButtonFactory.create(text: '4', color: Colors.white10, onPressed: () => buttonPressed('4')),
+                              CalcButtonFactory.create(text: '5', color: Colors.white10, onPressed: () => buttonPressed('5')),
+                              CalcButtonFactory.create(text: '6', color: Colors.white10, onPressed: () => buttonPressed('6')),
+                              CalcButtonFactory.create(text: '+', color: Colors.white24, onPressed: () => buttonPressed('+')),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          // calculator number buttons
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                //mainAxisAlignment: MainAxisAlignment.spaceAround
+                                children: [
+                                  Row(
+                                    children: [
+                                      CalcButtonFactory.create(text: '1', color: Colors.white10, onPressed: () => buttonPressed('1')),
+                                      SizedBox(
+                                          width: MediaQuery.of(context).size.width * 0.025),
+                                      CalcButtonFactory.create(text: '2', color: Colors.white10, onPressed: () => buttonPressed('2')),
+                                      SizedBox(
+                                          width: MediaQuery.of(context).size.width * 0.025),
+                                      CalcButtonFactory.create(text: '3', color: Colors.white10, onPressed: () => buttonPressed('3')),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      CalcButtonFactory.create(text: '+/-', color: Colors.white10, onPressed: () => buttonPressed('+/-')),
+                                      SizedBox(
+                                          width: MediaQuery.of(context).size.width * 0.025),
+                                      CalcButtonFactory.create(text: '0', color: Colors.white10, onPressed: () => buttonPressed('0')),
+                                      SizedBox(
+                                          width: MediaQuery.of(context).size.width * 0.025),
+                                      CalcButtonFactory.create(text: '.', color: Colors.white10, onPressed: () => buttonPressed('.')),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              CalcButtonFactory.create(text: '=', color: Colors.orange, onPressed: () => buttonPressed('=')),
+                            ],
+                          ),
                         ],
                       ),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          calcButton('+/-', Colors.white24,
-                                  () => buttonPressed('+/-')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.025),
-                          calcButton(
-                              '0', Colors.white24, () => buttonPressed('0')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.025),
-                          calcButton(
-                              '.', Colors.white24, () => buttonPressed('.')),
-                        ],
-                      ),
-                    ],
-                  ),
-                  calcButton('=', Colors.orange, () => buttonPressed('=')),
-                ],
-              ),
+                    )
+                ),
               ],
             )
-          ),
-      ],
-      )
-      )
+        )
+      ),
     );
   }
 
-  String equation = "0";
-  String result = "0";
-  String expression = "";
-  double equationFontSize = 38.0;
-  double resultFontSize = 48.0;
-
-  buttonPressed(String buttonText) {
-    // used to check if the result contains a decimal
-    String doesContainDecimal(dynamic result) {
-      if (result.toString().contains('.')) {
-        List<String> splitDecimal = result.toString().split('.');
-        if (!(int.parse(splitDecimal[1]) > 0)) {
-          return result = splitDecimal[0].toString();
-        }
-      }
-      return result;
-    }
-
-    setState(() {
-      if (buttonText == "AC") {
-        equation = "0";
-        result = "0";
-      } else if (buttonText == "⌫") {
-        equation = equation.substring(0, equation.length - 1);
-        if (equation == "") {
-          equation = "0";
-        }
-      } else if (buttonText == "+/-") {
-        if (equation[0] != '-') {
-          equation = '-$equation';
-        } else {
-          equation = equation.substring(1);
-        }
-      } else if (buttonText == "=") {
-        expression = equation;
-        expression = expression.replaceAll('×', '*');
-        expression = expression.replaceAll('÷', '/');
-        expression = expression.replaceAll('%', '%');
-
-        try {
-          Parser p = Parser();
-          Expression exp = p.parse(expression);
-
-          ContextModel cm = ContextModel();
-          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
-          if (expression.contains('%')) {
-            result = doesContainDecimal(result);
-          }
-        } catch (e) {
-          result = "Error";
-        }
-      } else {
-        if (equation == "0") {
-          equation = buttonText;
-        } else {
-          equation = equation + buttonText;
-        }
-      }
-    });
+  void buttonPressed(String value) {
+    final calculator = Provider.of<CalculatorProvider>(context, listen: false);
+    calculator.handleInput(value);
   }
 }
--- lib/calcButton.dart
+++ lib/calcButton.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
-Widget calcButton(
-    String buttonText, Color buttonColor, void Function()? buttonPressed) {
-  return Container(
-    width: 100,
-    height: buttonText == '=' ? 180 : 90,
-    padding: const EdgeInsets.all(6),
-    child: ElevatedButton(
-      onPressed: buttonPressed,
-      style: ElevatedButton.styleFrom(
-          shape: const RoundedRectangleBorder(
-              borderRadius:BorderRadius.all(Radius.circular(20))),
-          backgroundColor: buttonColor),
-      child: Text(buttonText,
-        style: const TextStyle(fontSize: 27, color: Colors.white),
+class CalcButtonFactory {
+  static Widget create({
+    required String text,
+    required Color color,
+    required VoidCallback onPressed,
+}){
+    return Container(
+      width: 100,
+      height: text == '=' ? 180 : 90,
+      padding: const EdgeInsets.all(6),
+      child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            backgroundColor: color,
+          ),
+        child: Text(
+          text, style: const TextStyle(fontSize: 27, color: Colors.white),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
